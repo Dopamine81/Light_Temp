d33e7ba86f8c7056e21e23b14a7dd421011990fe
Dopamine81 <mohammadjreza81@gmail.com>
2025-07-02 12:27:36 +0330

    Uploading The Trial Code

--- LightAndTemp/LightAndTemp.ino
+++ LightAndTemp/LightAndTemp.ino
@@ -1,12 +1,56 @@
-#include "TM1638"
+#include "DFRobot_SHT20.h";
+const int ledPin = LED_BUILTIN;
 
-void setup() {
+DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
+unsigned long br=0;
+const int maxbr = 255;
+const int minbr = 0;
 
-  // put your setup code here, to run once:
+
+void setup() 
+{
+
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+
+  sht20.initSHT20();
+  delay(100);
+  Serial.println("Temp Sensor");
+  sht20.checkSHT20();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  float humd = sht20.readHumidity();
+
+  float temp = sht20.readTemperature();
+
+  unsigned long Brightness = temp * 6;
+  br = Brightness;
+
+
+   
+  Serial.print("Time");
+  Serial.print(millis());
+  Serial.print("Temprature:");
+  Serial.print(temp, 1);
+  Serial.print("C");
+  Serial.print(" Humidity:");
+  Serial.print(humd, 1);
+  Serial.print("%");
+  Serial.println();
+  delay(1000);
+  Serial.print(br, 1);
+
+  if ( br <= maxbr) {
+    ledState = High;
+    analogWrite(ledPin, br);
+  }else{
+    ledState = LOW;
+  }
+
+
 
 }
